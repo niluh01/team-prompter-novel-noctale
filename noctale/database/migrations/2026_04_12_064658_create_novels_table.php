3adf328505411b238989e2530f9549176774a0f8
Amelia Syahroza <ameliaputrisyhrz@gmail.com>
2026-04-12 14:00:33 +0700

    add: migration database

---
+++ noctale/database/migrations/2026_04_12_064658_create_novels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('novels', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('cover')->nullable();
+            $table->string('status')->default('ongoing');
+            $table->string('publish_status')->default('published');
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('novels');
+    }
+};
---
+++ noctale/database/migrations/2026_04_12_065150_create_chapters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('novel_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->longText('content');
+            $table->integer('chapter_number');
+            $table->integer('views')->default(0);
+            $table->string('publish_status')->default('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
